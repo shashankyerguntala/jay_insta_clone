a8b3ca982f3e5b14ccac8a9619462ff069054bca
shashankyerguntala <shashankyerguntala27@gmail.com>
2025-10-17 12:45:23 +0530

    added_hex_format_colors

--- lib/core /constants/color_constants.dart
+++ lib/core /constants/color_constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ColorConstants {
   static const Color primaryColor = Color(0xFF0084FF);
   static const Color primaryLightColor = Color(0xFF42A5F5);
-  static const Color highlightColor = Color.fromARGB(124, 165, 215, 255);
+  static const Color highlightColor = Color(0x7CA5D7FF);
 
   static const Color backgroundColor = Color(0xFFFAFBFD);
   static const Color fillColor = Color(0xFFE8F4FF);
@@ -11,12 +11,12 @@ class ColorConstants {
   static const Color errorColor = Color(0xFFFF3B30);
   static const Color successColor = Color(0xFF34C759);
 
-  static const Color textPrimaryColor = Color.fromARGB(255, 7, 30, 70);///////////////////
+  static const Color textPrimaryColor = Color(0xFF071E46);
   static const Color textSecondaryColor = Color(0xFF6B7280);
   static const Color hintColor = Color(0xFF9CA3AF);
 
   static const Color borderColor = Color(0xFFD1E7FF);
   static const Color disabledColor = Color(0xFFE5E7EB);
 
-  static const Color transparent = Colors.transparent;
+  static const Color transparent = Color(0x00000000);
 }
